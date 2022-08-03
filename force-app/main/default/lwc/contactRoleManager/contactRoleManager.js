bcb2fbb75f09a6c288581435df37062d73ca7d6e
aleksei.sushkevich <aleksei.sushkevich@jetbi.com>
2022-08-03 16:02:24 +0300

    styles

--- force-app/main/default/lwc/contactRoleManager/contactRoleManager.js
+++ force-app/main/default/lwc/contactRoleManager/contactRoleManager.js
@@ -1,6 +1,9 @@
 import { LightningElement, track } from 'lwc';
+import { reduceErrors } from 'c/ldsUtils';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getAccordionData from '@salesforce/apex/ContactRoleManagerController.getAccordionData';
-import getContactRoles from '@salesforce/apex/ContactRoleManagerController.getContactRolesByObjId';
+import getContactRolesByObjId from '@salesforce/apex/ContactRoleManagerController.getContactRolesByObjId';
+import getNewContactRoleData from '@salesforce/apex/ContactRoleManagerController.getNewContactRoleData';
 
 const SLDS_IS_ACTIVE = "slds-is-active";
 const SLDS_NAV_ITEM = "slds-vertical-tabs__nav-item";
@@ -62,11 +65,21 @@ export default class ContactRoleManager extends LightningElement {
 
     //tabs
     @track tabs = [ACC, OPP, CASES, CONTR];
-    tabName = 'Accounts';
+    tabName = ACC.label;
+    notAccountTab = false;
+    notCaseTab = true;
+    notOppTab = true;
+    labelForInptut = '';
 
     //accordion
     @track accData;
     @track initialData;
+    @track filteredData;
+    selectedSection = false;
+    previousSectionValue = '';
+    downloadedData = true;
+    searchStringAccName = '';
+    searchStringLabel = '';
 
     //pagination
     nextDisabled;
@@ -76,20 +89,58 @@ export default class ContactRoleManager extends LightningElement {
     startingRecord = 1;
     endingRecord = 0;
     totalRecountCount;
-    pageSize = 11;
+    pageSize = 10;
 
     //table
+    @track accountValue;
     @track tableData;
+    @track contacts;
+    @track roleOptions;
+    @track fromOptions;
     recordId;
+    accountId;
+    showValues = 'All';
 
+    //utility
+    get showValue() {
+        return 'All';
+    }
+    get showOptions() {
+        return [
+            { label: 'All', value: 'All' },
+            { label: 'There are contact roles', value: 'There are contact roles' },
+            { label: 'There are no contact roles', value: 'There are no contact roles' },
+        ];
+    }
 
     connectedCallback(){
         this.getAccordionData();
     }
 
 
-    onSelectTabClick(event){
+    async onSelectTabClick(event){
         this.tabName = event.currentTarget.textContent;
+        if(this.tabName === ACC.label){
+            this.notAccountTab = false;
+            this.labelForInptut = '';
+            this.notCaseTab = true;
+            this.notOppTab = true;
+        }else if(this.tabName === OPP.label){
+            this.notAccountTab = true;
+            this.labelForInptut = 'Opportunity Name:';
+            this.notCaseTab = true;
+            this.notOppTab = false;
+        }else if(this.tabName === CASES.label){
+            this.notAccountTab = true;
+            this.labelForInptut = 'Case Number:';
+            this.notCaseTab = false;
+            this.notOppTab = true;
+        }else if(this.tabName === CONTR.label){
+            this.notAccountTab = true;
+            this.labelForInptut = 'Contract Number:';
+            this.notCaseTab = true;
+            this.notOppTab = true;
+        }
         this.tabs.forEach(element => {
             element.liClass = SLDS_NAV_ITEM;
             element.selected = "false";
@@ -98,11 +149,13 @@ export default class ContactRoleManager extends LightningElement {
                 element.selected = "true";
             }
         });
-        this.getAccordionData();
+        this.selectedSection = false;
+        await this.getAccordionData();
+        this.filterShow();
     }
 
     getAccordionData(){
-        getAccordionData({tabName : this.tabName})
+        return getAccordionData({tabName : this.tabName})
         .then(data => {
             this.accData = data.map(el=>{
                 return{
@@ -110,22 +163,19 @@ export default class ContactRoleManager extends LightningElement {
                     Label : el.Label,
                     Icon : "utility:chevronright",
                     isVisible : false,
-                    Class : "accordion"
+                    Class : "accordion",
+                    isSpin : false,
+                    notAcc : el.NotAcc,
+                    accName : el.AccountName,
+                    accId : el.AccountId,
+                    hasConRole : el.HasConRoles
                 }
             });
             this.initialData = this.accData;
             this.processRecords(this.accData);
         }).catch(error => {
-           this.error = error;
-           this.errorMessage = reduceErrors(this.error);
-           this.dispatchEvent(
-                new ShowToastEvent({
-                    title: ERROR_TITLE,
-                    message: this.errorMessage.toString(),
-                    variant: ERROR_VARIANT
-                })
-            );
-       });
+            this.catchFun(error);
+        });
     }
 
     processRecords(data){
@@ -171,43 +221,49 @@ export default class ContactRoleManager extends LightningElement {
     displayRecordPerPage(){
         this.startingRecord = (this.page -1) * this.pageSize;
         this.endingRecord = this.pageSize * this.page;
-
         this.endingRecord = (this.endingRecord > this.totalRecountCount) ? this.totalRecountCount : this.endingRecord; 
-
-        this.accData = this.initialData.slice(this.startingRecord, this.endingRecord);
-
+        if(this.searchStringAccName === '' && this.searchStringLabel === ''){
+            this.accData = this.initialData.slice(this.startingRecord, this.endingRecord);
+        }else{
+            this.accData = this.filteredData.slice(this.startingRecord, this.endingRecord);
+        }
     }
 
-    handleToggleSection(event){
-        this.recordId = event.currentTarget.dataset.id;
-        this.accData.forEach(el => {
-            if(el.Id === this.recordId){
-                el.Icon = el.Icon === "utility:chevrondown" ? "utility:chevronright" : "utility:chevrondown";
-                el.isVisible = el.isVisible ? false : true;
-                el.Class = el.Class === "accordion" ? "selected-accordion" : "accordion";
+    async handleToggleSection(event){
+        if(this.downloadedData){
+            this.recordId = event.currentTarget.dataset.id;
+            this.accountId = event.currentTarget.id.substring(0, event.currentTarget.id.length - 4);
+            const label = event.currentTarget.textContent;
+            if(!this.selectedSection){
+                await this.showTable(label);
             }else{
-                el.Icon = "utility:chevronright";
-                el.isVisible = false;
-                el.Class = "accordion";
+                if(this.previousSectionValue === label){
+                    this.closeTable();
+                }else{
+                    await this.showTable(label);
+                }
             }
-        });
-        this.getContactRoles();
+        }
     }
 
     getContactRoles(){
-        getContactRoles({objId : this.recordId})
+        return getContactRolesByObjId({objId : this.recordId})
         .then(data => {
-            console.log(data);
-            if(data.length !== 0){
+            let baseUrl = 'https://'+location.host+'/';
+            if(data != null && data.length !== 0){
                 this.tableData = data.map(el=>{
                     return{
                         id : el.Id,
-                        name : el.Name,
+                        namelabel : el.Name,
+                        namehref : baseUrl + el.ContactId,
                         title : el.Title,
-                        email : el.Email,
-                        phone : el.Phone,
+                        emailhref : "mailto:" + el.Email,
+                        emaillabel : el.Email,
+                        phonelabel : el.Phone,
+                        phonehref : "tel:" + el.Phone,
                         role : el.Role,
                         primary : el.Primary,
+                        showDropdownRole : false
                     }
                 });
             }else{
@@ -215,15 +271,167 @@ export default class ContactRoleManager extends LightningElement {
             }
         })
         .catch(error => {
-            this.error = error;
-            this.errorMessage = reduceErrors(this.error);
-            this.dispatchEvent(
-                 new ShowToastEvent({
-                    title: ERROR_TITLE,
-                    message: this.errorMessage.toString(),
-                    variant: ERROR_VARIANT
-                 })
-             );
+            this.catchFun(error);
         });
+    }
+
+    async showTable(label){
+        this.previousSectionValue = label;
+        this.downloadedData = false;
+        this.accData.forEach(el => {
+            if(el.Id === this.recordId){
+                el.Icon = el.Icon === "utility:chevrondown" ? "utility:chevronright" : "utility:chevrondown";
+                el.isSpin = el.isSpin ? false : true;
+                el.Class = el.Class === "accordion" ? "selected-accordion" : "accordion";
+            }else{
+                el.Icon = "utility:chevronright";
+                el.isSpin = false;
+                el.isVisible = false;
+                el.Class = "accordion";
+            }
+        });
+        await this.getContactRoles();
+        await this.getNewContactRoleData();
+        this.accData.forEach(el => {
+            if(el.Id === this.recordId){
+                el.isVisible = el.isVisible ? false : true;
+                el.isSpin = false;
+            }else{
+                el.isVisible = false;
+            }
+        });
+        this.accountValue = this.recordId;
+        this.downloadedData = true;
+        this.selectedSection = true;
+    }
+
+    closeTable(){
+        this.accData.forEach(el => {
+            el.Icon = "utility:chevronright";
+            el.isSpin = false;
+            el.isVisible = false;
+            el.Class = "accordion";
+        });
+        this.selectedSection = false;
+    }
+
+    getNewContactRoleData(){
+        return getNewContactRoleData({objId : this.recordId, accId : this.accountId})
+        .then(data => {
+            this.contacts = data.Contacts;
+            this.fromOptions = [{label : data.AccountName, value : this.recordId}, {label : 'All', value: 'All'}];
+            this.roleOptions = data.Roles.map(el=>{
+                return{
+                label : el,
+                value : el
+                };
+            });
+        })
+        .catch(error => {
+            this.catchFun(error);
+        });
+    }
+
+    catchFun(error){
+        this.error = error;
+        this.errorMessage = reduceErrors(this.error);
+        this.dispatchEvent(
+            new ShowToastEvent({
+            title: ERROR_TITLE,
+            message: this.errorMessage.toString(),
+            variant: ERROR_VARIANT
+            })
+        );
+    }
+
+    searchByAccName(event){
+        if(event !== undefined){
+            this.searchStringAccName = event.target.value;
+        }
+        let parseData = [];
+        if(this.showValues === 'All' || event === undefined){
+            parseData = JSON.parse(JSON.stringify(this.initialData));
+        }else{
+            this.filterShow();
+            parseData = JSON.parse(JSON.stringify(this.filteredData));
+        }
+        let newData = [];
+        for(var i = 0; i < parseData.length; i++){
+            if(parseData[i].accName.toLowerCase().trim().startsWith(this.searchStringAccName.toLowerCase().trim())){
+                newData.push(parseData[i]);
+            }
+        }
+        this.page = 1;
+        this.filteredData = newData;
+        this.processRecords(newData);
+    }
+
+    searchByLabel(event){
+        if(event !== undefined){
+            this.searchStringLabel = event.target.value;
+        }
+        let parseData = [];
+        if(this.showValues === 'All' || event === undefined){
+            parseData = JSON.parse(JSON.stringify(this.initialData));
+        }else{
+            this.filterShow();
+            parseData = JSON.parse(JSON.stringify(this.filteredData));
+        }
+        let newData = [];
+        for(var i = 0; i < parseData.length; i++){
+            if(parseData[i].Label.toLowerCase().trim().startsWith(this.searchStringLabel.toLowerCase().trim())){
+                newData.push(parseData[i]);
+            }
+        }
+        this.page = 1;
+        this.filteredData = newData;
+        this.processRecords(newData);
+    }
+
+    async handleChangeShow(event){
+        this.showValues = event.target.value;
+        await this.getAccordionData();
+        this.filterShow();
+    }
+
+    filterShow(){
+        let parseData = [];
+        if(this.searchStringAccName === '' && this.searchStringLabel === ''){
+            parseData = JSON.parse(JSON.stringify(this.initialData));
+        }else if(this.searchStringAccName !== '' && this.searchStringLabel === ''){
+            this.searchByAccName();
+            parseData = JSON.parse(JSON.stringify(this.filteredData));
+        }else if(this.searchStringAccName === '' && this.searchStringLabel !== ''){
+            this.searchByLabel();
+            parseData = JSON.parse(JSON.stringify(this.filteredData));
+        }else if(this.searchStringAccName !== '' && this.searchStringLabel !== ''){
+            this.searchByAccName();
+            this.searchByLabel();
+            parseData = JSON.parse(JSON.stringify(this.filteredData));
+        }
+        let newData = [];
+        if(this.showValues === 'There are contact roles'){
+            for(var i = 0; i < parseData.length; i++){
+                if(parseData[i].hasConRole){
+                    newData.push(parseData[i]);
+                }
+            }
+            this.filteredData = newData;
+            this.processRecords(newData);
+        }else if(this.showValues === 'There are no contact roles'){
+            for(var i = 0; i < parseData.length; i++){
+                if(!parseData[i].hasConRole){
+                    newData.push(parseData[i]);
+                }
+            }
+            this.filteredData = newData;
+            this.processRecords(newData);
+        }else{
+            for(var i = 0; i < parseData.length; i++){
+                newData.push(parseData[i]);
+            }
+            this.filteredData = newData;
+            this.processRecords(newData);
+        }
     }
 }
--- force-app/main/default/lwc/contactRoleTable/contactRoleTable.js
+++ force-app/main/default/lwc/contactRoleTable/contactRoleTable.js
@@ -1,11 +1,386 @@
 import { LightningElement, api, track} from 'lwc';
+import { reduceErrors } from 'c/ldsUtils';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import createNewContactRole from '@salesforce/apex/ContactRoleManagerController.createNewContactRole';
+import deleteContactRole from '@salesforce/apex/ContactRoleManagerController.deleteContactRole';
+import findContactByName from '@salesforce/apex/ContactRoleManagerController.findContactByName';
+import editContactRole from '@salesforce/apex/ContactRoleManagerController.editContactRole';
+import LightningConfirm from 'lightning/confirm';
+
+const ERROR_TITLE   = 'Error';
+const ERROR_VARIANT = 'error';
+const SUCCESS_VARIANT = 'success';
 
 export default class ContactRoleTable extends LightningElement {
+    //table
     @api tableData;
+    @api objId;
+    @api notCaseTab;
+    @api notOppTab;
+    editMode = false;
+    editRecords = [];
+    
+
+    //modal
     @track isModalOpen = false;
-    @track isEdit = true;
+    @api roleOptions;
+    editRoleOptions;
+    isPrimary = false;
+    selectedRole = '';
+    disabledSave = true;
+
+    //contact search
+    @api accountValue;
+    @api fromOptions;
+    @api contacts = [];
+    @track contactOptions;
+    searchPlace;
+    placeholder = 'Select a Contact';
+    searchString = '';
+    selectedContact = '';
+    selectedName = '';
+    showDropdown = false;
 
     handleNew(){
         this.isModalOpen = true;
+        this.contactOptions = this.contacts.map(el =>{
+            return{
+                label : el.Name,
+                value : el.Id,
+                isVisible : true
+            }
+        });
+    }
+
+    closeModal(){
+        this.isModalOpen = false;
+        this.searchString = '';
+        this.selectedContact = '';
+        this.selectedName = '';
+        this.selectedRole = '';
+        this.disabledSave = true;
+        this.isPrimary = false;
+        this.placeholder = 'Select a Contact';
+    }
+
+    showOptions() {
+        this.showDropdown = true;
+        if(this.selectedName !== ''){
+            this.searchString = '';
+            this.placeholder = this.selectedName;
+        }
+    }
+
+    blurEvent() {
+        this.showDropdown = false;
+        if(this.selectedName !== ''){
+            this.searchString = this.selectedName;
+            this.placeholder = '';
+        }
+        for(var i = 0; i < this.contactOptions.length; i++) {
+            this.contactOptions[i].isVisible = true;
+        }
+    }
+
+    selectItem(event){
+        this.selectedContact = event.currentTarget.dataset.id;
+        this.selectedName = event.currentTarget.textContent;
+        this.searchString = event.currentTarget.textContent;
+        this.showDropdown = false;
+        this.checkDesable();
+    }
+
+    filterOptions(event) {
+        this.searchString = event.target.value;
+        if(this.searchPlace !== 'All'){
+            this.searchFun();
+        }else{
+            findContactByName({searchTerm : this.searchString})
+            .then(data=>{
+                this.contactOptions = data.map(el=>{
+                    return{
+                        label : el.Name,
+                        value : el.Id,
+                        isVisible : true
+                    }
+                })
+            })
+            .catch(error=>{
+                this.catchFun(error);
+            });
+        }
+    }
+
+    searchFun(){
+        if(this.searchString.length >= 1) {
+            for(var i = 0; i < this.contactOptions.length; i++) {
+                if(this.contactOptions[i].label.toLowerCase().trim().startsWith(this.searchString.toLowerCase().trim())) {
+                    this.contactOptions[i].isVisible = true;
+                } 
+                else {
+                    this.contactOptions[i].isVisible = false;
+                }
+            }
+            this.showDropdown = true;
+        }else{
+            for(var i = 0; i < this.contactOptions.length; i++) {
+                this.contactOptions[i].isVisible = true;
+            }
+        }
+    }
+
+    checkDesable(){
+        if(this.selectedContact === '' || this.selectedRole === ''){
+            this.disabledSave = true;
+        }else{
+            this.disabledSave = false;
+        }
+    }
+
+    handleChangePrimary(event){
+        this.isPrimary = event.target.checked;
+    }
+
+    handleChangeRole(event){
+        this.selectedRole = event.target.value;
+        this.checkDesable();
+    }
+
+    onSave(){
+        createNewContactRole({objId : this.objId, isPrimary : this.isPrimary, selectedContact : this.selectedContact, selectedRole: this.selectedRole})
+        .then(()=>{
+            this.closeModal();
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    message:'Contact Role has been created.',
+                    variant: SUCCESS_VARIANT
+                })
+            );
+            this.dispatchEvent(new CustomEvent('updatedata'));
+        }).catch(error =>{
+            this.catchFun(error);
+        });
+    }
+    
+    catchFun(error){
+        this.error = error;
+        this.errorMessage = reduceErrors(this.error);
+        this.dispatchEvent(
+            new ShowToastEvent({
+            title: ERROR_TITLE,
+            message: this.errorMessage.toString(),
+            variant: ERROR_VARIANT
+            })
+        );
+    }
+
+    handleChangeSearch(event){
+        if(event.target.value === 'All'){
+            findContactByName({searchTerm : this.searchString})
+            .then(data=>{
+                this.contactOptions = data.map(el=>{
+                    return{
+                        label : el.Name,
+                        value : el.Id,
+                        isVisible : true
+                    }
+                })
+            })
+            .catch(error=>{
+                this.catchFun(error);
+            });
+        }else{
+            this.contactOptions = this.contacts.map(el =>{
+                return{
+                    label : el.Name,
+                    value : el.Id,
+                    isVisible : true
+                }
+            });
+        }
+        this.searchPlace = event.target.value;
+    }
+
+    async deleteContactRole(event){
+        const curId = event.currentTarget.id.substring(0, event.currentTarget.id.length - 4);
+        const itemIndex = this.findIndexById(curId);
+        let deleteAssignment = 
+            `Remove ${this.tableData[itemIndex].namelabel} from ${this.fromOptions[0].label}.`;
+        const result = await LightningConfirm.open({
+            message: '',
+            variant: 'header',
+            label: deleteAssignment,
+            theme: 'error',
+        });
+        if(result){
+            deleteContactRole({objId: this.objId, id: this.tableData[itemIndex].id})
+            .then(()=>{
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        message: `${this.tableData[itemIndex].namelabel} from ${this.fromOptions[0].label} has been deleted.`,
+                        variant: SUCCESS_VARIANT
+                    })
+                );
+                this.dispatchEvent(new CustomEvent('updatedata'));
+            })
+            .catch(error=>{
+                this.catchFun(error);
+            });
+        }
+    }
+
+    findIndexById(id){
+        for(var i = 0; i < this.tableData.length; i++) {
+            if(id === this.tableData[i].id){
+                return i;
+            }
+        }
+        return null;
+    }
+
+    onEdit(){
+        if(this.tableData.length > 0){
+            this.editMode = this.editMode ? false : true;
+        }
+        if(this.editRecords.length > 0){
+            this.editRecords = [];
+            this.dispatchEvent(new CustomEvent('updatedata'));
+        }
+    }
+
+    showRoleOptions(event){
+        const curId = event.currentTarget.id.substring(0, event.currentTarget.id.length - 4);
+        const itemIndex = this.findIndexById(curId);
+        const value = event.currentTarget.textContent;
+        this.editRoleOptions = this.roleOptions.map(el=>{
+            return{
+                label : el.label,
+                value : el.value,
+                icon  : false
+            };
+        });
+        for(var i = 0; i < this.editRoleOptions.length; i++){
+            if(this.editRoleOptions[i].label === value){
+                this.editRoleOptions[i].icon = true;
+            }
+        }
+        this.editRoleOptions.sort(function(x,y){ 
+            return x.label == value ? -1 : y == value ? 1 : 0; 
+        });
+        let condition = this.tableData[itemIndex].showDropdownRole;
+        let data = this.tableData.map(el=>{
+            return{
+                id : el.id,
+                namelabel : el.namelabel,
+                namehref : el.namehref,
+                title : el.title,
+                emailhref : el.emailhref,
+                emaillabel : el.emaillabel,
+                phonelabel : el.phonelabel,
+                phonehref : el.phonehref,
+                role : el.role,
+                primary : el.primary,
+                showDropdownRole : false
+            }
+        });
+        data[itemIndex].showDropdownRole = condition ? false : true;
+        this.tableData = data;
+    }
+
+    changeRole(event){
+        const value = event.currentTarget.textContent;
+        const curId = event.currentTarget.id.substring(0, event.currentTarget.id.length - 4);
+        let exist = false;
+        let index = 0;
+        for(var i = 0; i < this.editRecords.length; i++){
+            if(this.editRecords[i].Id === curId){
+                exist = true;
+                index = i;
+            }
+        }
+        if(exist){
+            this.editRecords[index].Role = value;
+        }else{
+            this.editRecords.push({Id : curId, Role : value});
+        }
+        const itemIndex = this.findIndexById(curId);
+        this.tableData[itemIndex].role = value;
+    }
+
+    changePrimary(event){
+        const curId = event.currentTarget.id.substring(0, event.currentTarget.id.length - 4);
+        const itemIndex = this.findIndexById(curId);
+        const value = event.target.checked;
+        let data = [];
+        if(this.notOppTab){
+            data = this.tableData.map(el=>{
+                return{
+                    id : el.id,
+                    namelabel : el.namelabel,
+                    namehref : el.namehref,
+                    title : el.title,
+                    emailhref : el.emailhref,
+                    emaillabel : el.emaillabel,
+                    phonelabel : el.phonelabel,
+                    phonehref : el.phonehref,
+                    role : el.role,
+                    primary : false,
+                    showDropdownRole : false
+                }
+            });
+        }else{
+            data = this.tableData.map(el=>{
+                return{
+                    id : el.id,
+                    namelabel : el.namelabel,
+                    namehref : el.namehref,
+                    title : el.title,
+                    emailhref : el.emailhref,
+                    emaillabel : el.emaillabel,
+                    phonelabel : el.phonelabel,
+                    phonehref : el.phonehref,
+                    role : el.role,
+                    primary : el.primary,
+                    showDropdownRole : false
+                }
+            });
+        }
+        data[itemIndex].primary = value;
+        this.tableData = data;
+        let exist = false;
+        let index = 0;
+        for(var i = 0; i < this.editRecords.length; i++){
+            if(this.editRecords[i].IsPrimary !== undefined && this.notOppTab){
+                this.editRecords[i].IsPrimary = false;
+            }
+            if(this.editRecords[i].Id === curId){
+                exist = true;
+                index = i;
+            }
+        }
+        if(exist){
+            this.editRecords[index].IsPrimary = value;
+        }else{
+            this.editRecords.push({Id : curId, IsPrimary : value});
+        }
+    }
+    
+    onSaveEdit(){
+        const message = this.editRecords.length > 1 ? `Records have been changed.` : `Record have been changed.`;
+        editContactRole({valuesToUpdate : this.editRecords})
+        .then(()=>{
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    message: message,
+                    variant: SUCCESS_VARIANT
+                })
+            );
+            this.editRecords = [];
+            this.editMode = this.editMode ? false : true;
+            this.dispatchEvent(new CustomEvent('updatedata'));
+        })
+        .catch(error=>{
+            this.catchFun(error);
+        });
     }
 }
